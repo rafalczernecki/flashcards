09ce21639b46dbb4049d2ddfcab6aa6c1b20ae88
RCZERNEC <rafal.czernecki@camgemini.com>
2020-05-19 15:59:44 +0200

    db connection setup

--- server/app.js
+++ server/app.js
@@ -1,19 +1,32 @@
 const express = require('express');
-const bodyParser = require ('body-parser');
+const bodyParser = require('body-parser');
 
 const flashcardsRoutes = require('./routes/flashcards');
 
 const app = express();
+const mongoose = require('mongoose');
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
 
 app.use('', flashcardsRoutes);
 
-app.listen(8096);
+mongoose
+  .connect(
+    'mongodb+srv://user:user@cluster0-5maxx.mongodb.net/flashcards?retryWrites=true&w=majority'
+  )
+  .then(result => {
+      app.listen(8080);
+  })
+  .catch((err) => {
+      console.log('err');
+  });
