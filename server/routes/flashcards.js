ffc65de3f0d079d927c9afac23b4e5da8173f1b7
RCZERNEC <rafal.czernecki@camgemini.com>
2020-05-30 13:53:43 +0200

    add no translation checked validation message

--- server/routes/flashcards.js
+++ server/routes/flashcards.js
@@ -13,7 +13,7 @@ router.post('/save', [
     body('dictionary').trim().isLength({min:4, max: 4}),
     body('originalLang').trim().isLength({min:2, max:2}),
     body('word').trim().isLength({min: 1, max: 30}),
-    body('translations').isLength({min: 1, max: 50})
+    body('translations').isLength({min: 1, max: 50}).withMessage('At least one translation must be matched')
 ], flashcardsController.postSave);
 
 router.post('', flashcardsController.postFlashcards);
